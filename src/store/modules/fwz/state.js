2d4aadc6204f62de3a718c13597fc300625d733f
xiaobudongzhang <xiaobudongzhang@gmail.com>
2017-04-18 16:27:29 +0800

    update chart

--- src/store/modules/fwz/state.js
+++ src/store/modules/fwz/state.js
@@ -10,6 +10,11 @@ date.setDate(date.getDate()-6);
 var weekStart=dateFormat(date,'yyyy-mm-dd');
 
 var opt={
+    yAxis: {
+	allowDecimals: 'false',        //控制数轴是否显示小数。
+	//min: 0,                                //控制数轴的最小值
+	//max: 10000,                        //控制数轴的最大值
+    },
                 chart: {
             type: 'line',
             zoomType: 'x'
